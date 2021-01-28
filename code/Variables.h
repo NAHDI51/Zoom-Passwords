cb0cc47b035fe9e17e5339cc81d5b65b50b8a203
NAHDI51 <hossainnahdi8051@gmail.com>
2021-01-28 18:18:28 +0600

    added the first one

---
+++ code/Variables.h
@@ -0,0 +1,37 @@
+/*
+<Variables.h>
+
+this header file defines all the global variables to be used throughout the codes of the project.
+Feel free to change.
+*/
+
+#ifndef _STDIO_H_
+#include <stdio.h>
+#endif //includes stdio.h if not added previously.
+
+#ifndef _STDLIB_H_
+#include <stdlib.h>
+#endif //includes stdlib.h if not added previously.
+
+#ifndef _STRING_H_
+#include <string.h>
+#endif //includes string.h if not added previously.
+
+#ifndef _CTYPE_H_
+#include <ctype.h>
+#endif //includes ctype.h if not added previously.
+
+//So basically our structure will be called list.
+typedef struct creditentials list;
+typedef char* name_of;
+typedef list* list_p;
+int content_count = 0;              //Keeps track of the number of subjects.
+
+struct creditentials{
+    name_of *teacher;
+    name_of subject;
+    char **ID;
+    char **pass;
+    list_p prev, next;
+    int entries;                    //Entries can't be bigger than 10, no matter what.
+};
